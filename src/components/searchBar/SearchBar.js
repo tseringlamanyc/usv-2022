cdf67908c405e5fca670c06536526df5bf4c4d01
Tsering Lama <tseringglamaa@gmail.com>
2022-06-24 00:27:15 -0400

    minor fix

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -5,10 +5,6 @@ import "./SearchBar.scss";
 function SearchBar({ searchHandler }) {
   return (
     <div className="bar">
-      <div className="bar_image">
-        <img src="https://img.freepik.com/free-photo/food-collage-dishes-meat-fish-vegetables-black-stone-background_187166-40735.jpg?w=2000" />
-      </div>
-
       <div className="bar_search">
         <SearchIcon className="icon" />
         <input placeholder="Search" onChange={searchHandler}></input>
